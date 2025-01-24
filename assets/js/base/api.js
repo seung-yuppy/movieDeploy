5f75e2b61c8393f089c5a079ceb4f0f411ee2dab
seung-yuppy <songsy0107@gmail.com>
2025-01-24 21:59:27 +0900

    25/01/24 - api 키 바꾸기

--- assets/js/base/api.js
+++ assets/js/base/api.js
@@ -1,7 +1,7 @@
 const api = {
-    API_KEY: '2d7b9efb',
+    API_KEY: '2326d4aa',
     BASE_URL: 'https://www.omdbapi.com/',
-    GIT_URL: 'https://2eebyeonghyun.github.io/Est5movie',
+    GIT_URL: 'https://seung-yuppy.github.io/movieDeploy',
     TMDB_KEY: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjJiMTFhZWYzZjg1YmQ1N2ViMTdlYTc2YjIxYTg5MyIsIm5iZiI6MTcwMjUzMTcxMS40MjcsInN1YiI6IjY1N2E5MjdmN2EzYzUyMDBlYmZlZmNiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qlrj84jcEANg4pdpPQLeIYdKBipFGoUfLUAm44BGNLw'
 }
 

5819b980edf37e860aedbf61db63a44622fbce54
seung-yuppy <songsy0107@gmail.com>
2025-01-24 22:09:26 +0900

    해상도 변경

--- assets/js/base/Highimg.js
+++ assets/js/base/Highimg.js
@@ -1,7 +1,7 @@
 export function getPoster(poster) {
     let highPoster;
     if (movie.Poster !== '') {
-        highPoster = movie.Poster.replace("SX300", "SX3000");
+        highPoster = movie.Poster.replace("SX300", "SX300");
     }
     return highPoster;
 }

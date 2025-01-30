211e9d163d10bc74bf4d55836d7b817878caa58d
seung-yuppy <songsy0107@gmail.com>
2025-01-30 19:39:07 +0900

    ios수정

--- assets/js/public/main.js
+++ assets/js/public/main.js
@@ -25,7 +25,7 @@ async function mainSlide() {
             item.innerHTML = 
             `
                 <div class="movie-imgBox">
-                    <img src="${api.GIT_URL}/assets/json/main.json/${movie.Poster}">
+                    <img src="${api.GIT_URL}/${movie.Poster}">
                 </div>
                 <div class="movie-informationBox">
                     <h2 class="movie-title">${movie.Title}</h2>
